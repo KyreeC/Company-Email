f377d9fe9f7219d6e9b901ad3c5f4a91674f441c
Kyree Cook <KyreeCook2019@gmail.com>
2022-06-30 03:47:01 -0400

    Program Change

--- Company Email.py
+++ Company Email.py
@@ -1,9 +1,14 @@
 name = input("Enter your name: ")
 Last_Name = input("Enter your last name: ")
 age = input("Enter your age: ")
-
 New_Email = name[0] + Last_Name + "@Company.com"
 
-print(New_Email)
+if age >= 16:
+    print("Your email is " + New_Email)
+else:
+    sign = input("I need an adult signature for your account to be verified, please enter your guardian signature here: ")
+    if sign == True:
+        print("Your email is " + New_Email)
+    else:
+        print("Invalid signature, please contact your administrator.")
 
-print(name[0] + Last_Name[0])
