ac719edbd9f38285c1626f8423070cc6394caad3
Kyree Cook <KyreeCook2019@gmail.com>
2022-06-30 03:41:57 -0400

    Company Email Program

---
+++ Company Email.py
@@ -0,0 +1,9 @@
+name = input("Enter your name: ")
+Last_Name = input("Enter your last name: ")
+age = input("Enter your age: ")
+
+New_Email = name[0] + Last_Name + "@Company.com"
+
+print(New_Email)
+
+print(name[0] + Last_Name[0])
